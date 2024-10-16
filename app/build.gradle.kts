982acf4e5558b871a625dc992a407618c53fbbee
Chattercube <33176933+Chattercube@users.noreply.github.com>
2024-10-16 12:16:52 +0800

    Building login datasource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import org.apache.tools.ant.util.JavaEnvUtils.VERSION_11
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 
 }
 
--- app/src/main/java/com/example/hkunexus/data/LoginDataSource.kt
+++ app/src/main/java/com/example/hkunexus/data/LoginDataSource.kt
@@ -1,18 +1,37 @@
 package com.example.hkunexus.data
 
 import com.example.hkunexus.data.model.LoggedInUser
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.auth.user.UserInfo
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import kotlinx.coroutines.withTimeoutOrNull
 import java.io.IOException
+import java.util.concurrent.TimeoutException
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 class LoginDataSource {
 
-    fun login(username: String, password: String): Result<LoggedInUser> {
+    val supabase = createSupabaseClient(
+        supabaseUrl = "https://ctiaasznssbnyizmglhv.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWFhc3puc3Nibnlpem1nbGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1NDQ3NzEsImV4cCI6MjA0NDEyMDc3MX0.t0-AHECeFc0PWItTVJ-X0BGGclh_LEbFhFOtBi9rNd4"
+    ) {
+        install(Postgrest)
+        install(Auth)
+    }
+
+    suspend fun login(email: String, password: String): Result<UserInfo> {
         try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            val result = supabase.auth.signInWith(Email) {
+                this.email = email;
+                this.password = password
+            };
+            val user = supabase.auth.retrieveUserForCurrentSession(updateSession = true)
+            return Result.Success(user)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
         }
