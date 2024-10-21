259b3a3c47895d5a2371587dcf6166a9c0cadc29
Chattercube <33176933+Chattercube@users.noreply.github.com>
2024-10-21 11:30:43 +0800

    FIX FIX FIX

--- app/src/main/java/com/example/hkunexus/data/LoginDataSource.kt
+++ app/src/main/java/com/example/hkunexus/data/LoginDataSource.kt
@@ -19,8 +19,7 @@ class LoginDataSource {
 
     val supabase = createSupabaseClient(
         supabaseUrl = "https://ctiaasznssbnyizmglhv.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWFhc3puc3Nibnlpem1nbGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1NDQ3NzEsImV4cCI6MjA0NDEyMDc3MX0.t0-AHECeFc0PWItTVJ-X0BGGclh_LEbFhFOtBi9rNd4"
-    ) {
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWFhc3puc3Nibnlpem1nbGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1NDQ3NzEsImV4cCI6MjA0NDEyMDc3MX0.t0-AHECeFc0PWItTVJ-X0BGGclh_LEbFhFOtBi9rNd4"    ) {
         install(Postgrest)
         install(Auth)
     }
