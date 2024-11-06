e1cbbd9eeddd950f184f3661ff2f90dfec15b3f0
boriswong <u3606591@connect.hku.hk>
2024-11-06 14:32:10 +0800

    make the simple version of fragment_group_landing.xml

--- app/src/main/java/com/example/hkunexus/MainActivity.kt
+++ app/src/main/java/com/example/hkunexus/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.hkunexus
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.content.Intent
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -11,7 +15,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.hkunexus.databinding.ActivityMainBinding
+import com.example.hkunexus.ui.login.LoginActivity
+import com.example.hkunexus.data.SupabaseSingleton
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,21 +38,50 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-//        val dfrag = DashboardFragment()
-//        dfrag.accessToken = SupabaseSingleton.getAccessToken()
-//        dfrag.mainActivity = this;
-//        addFragment( dfrag, false, "Dashboard")
+        //val dfrag = DashboardFragment()
+        //dfrag.accessToken = SupabaseSingleton.getAccessToken()
+        //dfrag.mainActivity = this;
+        //addFragment(dfrag, false, "Dashboard")
 
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
+        val navView: NavigationView = binding.navView
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_logout -> {
+                    // Start LoginActivity instead of replacing a Fragment
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    // Optionally finish the current activity if you don't want to return
+                    finish()
+                    true // Indicate that the item selection was handled
+                }
+
+                else -> false // Allow other items to be handled if necessary
+            }
+        }
+
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val bottomNavView: BottomNavigationView = binding.bottomNavView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -59,7 +95,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavView.setupWithNavController(navController)
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
     }
