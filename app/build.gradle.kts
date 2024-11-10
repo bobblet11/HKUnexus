98c4246008efea6b5fe518cce04abd56cd7353f8
bobblet11 <benglover78@gmail.com>
2024-11-10 22:35:42 +0800

    added refresh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.glide)
-    annotationProcessor(libs.compiler) // For Java
 
 }
