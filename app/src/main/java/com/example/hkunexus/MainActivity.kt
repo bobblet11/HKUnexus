1a3e961c8d9b240219b48ef3bdeef7bde7977ab3
Chattercube <33176933+Chattercube@users.noreply.github.com>
2024-11-10 16:33:35 +0800

    Revert "fixed bug for the menu item and the fragment change too"
    This reverts commit cc6ac91b

--- app/src/main/java/com/example/hkunexus/MainActivity.kt
+++ app/src/main/java/com/example/hkunexus/MainActivity.kt
@@ -1,84 +1,88 @@
 package com.example.hkunexus
 
-import MyEventsSlide
 import android.os.Bundle
 import android.view.MenuItem
 import android.content.Intent
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.example.hkunexus.data.SupabaseSingleton
-import com.example.hkunexus.data.UserSingleton
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.hkunexus.databinding.ActivityMainBinding
-import com.example.hkunexus.ui.homePages.create.CreateSlideFragment
-import com.example.hkunexus.ui.homePages.explore.ExploreFragment
-import com.example.hkunexus.ui.homePages.home.HomeFragment
-
-import com.example.hkunexus.ui.homePages.mygroups.MyGroupsFragment
 import com.example.hkunexus.ui.login.LoginActivity
+import com.example.hkunexus.data.SupabaseSingleton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var toolbar: Toolbar
+class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout : DrawerLayout
-    private lateinit var bottomNavView: BottomNavigationView
+
+    fun addFragment(fragment: Fragment?, addToBackStack: Boolean, tag: String?) {
+        val manager: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = manager.beginTransaction()
+        if (addToBackStack) {
+            ft.addToBackStack(tag)
+        }
+        if (fragment != null) {
+            ft.replace(R.id.fragmentFrame, fragment, tag)
+        }
+        ft.commitAllowingStateLoss()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        //val dfrag = DashboardFragment()
+        //dfrag.accessToken = SupabaseSingleton.getAccessToken()
+        //dfrag.mainActivity = this;
+        //addFragment(dfrag, false, "Dashboard")
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        drawerLayout = binding.drawerLayout
-
-        toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        val toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            toolbar,
-            R.string.open_nav,
-            R.string.close_nav
-        )
-
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
         val navView: NavigationView = binding.navView
-        navView.setNavigationItemSelectedListener(this)
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_logout -> {
+                    // Start LoginActivity instead of replacing a Fragment
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    // Optionally finish the current activity if you don't want to return
+                    finish()
+                    true // Indicate that the item selection was handled
+                }
 
-        val headerView : View = navView.getHeaderView(0)
-        val navUsername : TextView = headerView.findViewById(R.id.usernameAside)
-        val navUserEmail : TextView = headerView.findViewById(R.id.emailAside)
-
-        navUsername.text = UserSingleton.display_name
-        navUserEmail.text = UserSingleton.email
-
-
-        bottomNavView = binding.bottomNavView
-        bottomNavView.setOnItemSelectedListener{ item ->
-            when (item.itemId) {
-                R.id.navigation_home -> openFragment(HomeFragment(), "Home")
-                R.id.navigation_explore -> openFragment(ExploreFragment(), "Explore")
-                R.id.navigation_create -> openFragment(CreateSlideFragment(), "Create")
-                R.id.navigation_my_events -> openFragment(MyEventsSlide(), "My Events")
-                R.id.navigation_my_groups -> openFragment(MyGroupsFragment(), "My Groups")
+                else -> false // Allow other items to be handled if necessary
             }
-            true
         }
-        openFragment(HomeFragment(),"Home")
 
-        /* val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+//
+//        val drawerLayout: DrawerLayout = binding.drawerLayout
+//        val toggle = ActionBarDrawerToggle(
+//            this,
+//            drawerLayout,
+//            toolbar,
+//            R.string.open_nav,
+//            R.string.close_nav
+//        )
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+
+        val bottomNavView: BottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -93,37 +97,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
 
-        actionBar?.setDisplayHomeAsUpEnabled(true)*/
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_logout -> {
-                val intent = Intent(this, LoginActivity::class.java)
-                SupabaseSingleton.logout()
-                startActivity(intent)
-            }
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            onBackPressedDispatcher.onBackPressed()
-        }
-    }
-
-    private fun openFragment(fragment: Fragment, name: String){
-        getSupportActionBar()?.setTitle(name);
-        val manager: FragmentManager = supportFragmentManager
-        manager.beginTransaction().replace(R.id.fragmentFrame, fragment).commit()
-
-    }
-
-   /* override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
             android.R.id.home -> {
@@ -133,6 +110,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             else -> return super.onOptionsItemSelected(item)
         }
-    }*/
+    }
 
 }
