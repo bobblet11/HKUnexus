bab32d75b08f64fbab407be83f53e6f3d38483ad
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2024-11-11 19:19:18 +0800

    Post form reset

--- app/src/main/java/com/example/hkunexus/ui/homePages/create/CreatePostViewModel.kt
+++ app/src/main/java/com/example/hkunexus/ui/homePages/create/CreatePostViewModel.kt
@@ -13,19 +13,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.Calendar
 import java.util.UUID
-
-data class MyGroupsUiState(
-    val selectedClub: ClubDto? = null,
-    val isClubSelected: Boolean = false,
-    val isEventPost: Boolean = false,
-    val isEventSelected: Boolean = false,
-    val selectedEvent: EventDto? = null,
-    val isPostValid: Boolean = false,
-    val postTitle: String = "",
-    val postBody: String = "",
-
-)
 
 val MIN_TITLE_LENGTH = 10
 val MAX_TITLE_LENGTH = 20
@@ -34,6 +23,18 @@ val MIN_BODY_LENGTH = 20
 val MAX_BODY_LENGTH = 100
 
 class CreatePostViewModel: ViewModel() {
+    data class MyGroupsUiState(
+        val selectedClub: ClubDto? = null,
+        val isClubSelected: Boolean = false,
+        val isEventPost: Boolean = false,
+        val isEventSelected: Boolean = false,
+        val selectedEvent: EventDto? = null,
+        val isPostValid: Boolean = false,
+        val postTitle: String = "",
+        val postBody: String = "",
+
+        )
+
     private val _uiState = MutableStateFlow(MyGroupsUiState())
     val uiState: StateFlow<MyGroupsUiState> = _uiState.asStateFlow()
 
@@ -150,6 +151,21 @@ class CreatePostViewModel: ViewModel() {
             return true
         }
         return false
+    }
+
+    fun reset() {
+        _uiState.update {
+            it.copy(
+                selectedClub = null,
+                isClubSelected = false,
+                isEventPost = false,
+                isEventSelected = false,
+                selectedEvent = null,
+                isPostValid = false,
+                postTitle = "",
+                postBody = "",
+            )
+        }
     }
 }
 
