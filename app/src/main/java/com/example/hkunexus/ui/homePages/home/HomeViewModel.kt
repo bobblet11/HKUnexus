e8ef582d085888a21288f6f436a4e4948aea395c
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2024-11-11 22:12:19 +0800

    Fixed night theme problems

--- app/src/main/java/com/example/hkunexus/ui/homePages/home/HomeViewModel.kt
+++ app/src/main/java/com/example/hkunexus/ui/homePages/home/HomeViewModel.kt
@@ -33,7 +33,7 @@ class HomeViewModel : ViewModel() {
         }
     }
 
-    public fun fetchPosts() {
+    fun fetchPosts() {
         val tempList: Array<PostDto> = SupabaseSingleton.getPostsFromHome().toTypedArray()
         Log.d("homeViewModel", tempList.toString())
         updateHomePosts(tempList)
