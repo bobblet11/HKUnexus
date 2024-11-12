97d3670fca48a84b1d1a08f007bf29ffdea0a69e
bobblet11 <benglover78@gmail.com>
2024-11-12 16:15:13 +0800

    added Naviagation and post page

---
+++ app/src/main/java/com/example/hkunexus/ui/homePages/post/PostViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.hkunexus.ui.homePages.post
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.hkunexus.data.SupabaseSingleton
+import com.example.hkunexus.data.model.dto.PostDto
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+data class PostInfoState(
+    val postId: String = "",
+    val postTitle: String = "",
+)
+
+
+// Taken from MyEventsViewModel
+class PostViewModel : ViewModel() {
+
+    private val _uiStatePosts = MutableStateFlow(PostInfoState())
+    val uiStatePosts: StateFlow<PostInfoState> = _uiStatePosts.asStateFlow()
+
+    private fun updatePostInfo(newPosts: PostDto) {
+        _uiStatePosts.update {
+            it.copy(
+                postTitle = newPosts.title
+            )
+        }
+    }
+
+    public fun fetchPosts() {
+        val post: PostDto? = SupabaseSingleton.getPostById(uiStatePosts.value.postId)
+        Log.d("homeViewModel", post.toString())
+        updatePostInfo(post!!)
+    }
+
+    public fun setPostID(id:String?) {
+        _uiStatePosts.update {
+            it.copy(
+                postId = id!!
+            )
+        }
+    }
+
+}
